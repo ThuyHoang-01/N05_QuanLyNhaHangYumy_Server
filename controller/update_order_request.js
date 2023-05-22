e8bc6818885108fbd02255ab0d8f741f921462f8
thuyhoang11 <gianglamweb@gmail.com>
2023-05-22 18:54:05 +0700

    commit

--- controller/update_order_request.js
+++ controller/update_order_request.js
@@ -1,13 +1,23 @@
 const expressAsyncHandler = require("express-async-handler");
 const connection = require("../database/connect");
 
-const update_order_request= expressAsyncHandler(async (req, res)=> {
-    try {
-        const [rows]= await connection.execute("UPDATE order_request SET user_name= ?, phone= ?, email= ?, deposit= ? WHERE order_request_id= ?",[req.body.user_name, req.body.phone, req.body.email, req.body.deposit, req.body.order_request] )
-        return res.status(200).json({update: true})
-    } catch (error) {
-        return res.status(201).json(error)
-    }
-})
+const update_order_request = expressAsyncHandler(async (req, res) => {
+  try {
+    const [rows] = await connection.execute(
+      "UPDATE order_request SET user_name= ?, phone= ?, email= ?, deposit= ?, amount_deposit= ? WHERE order_request_id= ?",
+      [
+        req.body.user_name,
+        req.body.phone,
+        req.body.email,
+        req.body.deposit,
+        req.body.amount_deposit || 0,
+        req.body.order_request,
+      ]
+    );
+    return res.status(200).json({ update: true });
+  } catch (error) {
+    return res.status(201).json(error);
+  }
+});
 
-module.exports= update_order_request
+module.exports = update_order_request;
